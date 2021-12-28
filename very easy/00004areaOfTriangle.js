3641b10cea8a03f6a4d171f7b48cb0e7b585ce94
Torjuman <torjumanrimon7@gmail.com>
2021-12-28 15:23:03 +0600

    Fourth problem solved

---
+++ very easy/00004areaOfTriangle.js
@@ -0,0 +1,8 @@
+function triArea(triBase, triHeight) {
+  let result = (1 / 2) * triBase * triHeight;
+  return result;
+}
+
+console.log(triArea(3, 2));
+console.log(triArea(7, 4));
+console.log(triArea(10, 10));
