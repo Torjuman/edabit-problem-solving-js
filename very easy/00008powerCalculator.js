b45866c64dce285d9b06aee4af2eb6d8f21f442f
Torjuman <torjumanrimon7@gmail.com>
2021-12-28 15:57:52 +0600

    Eigth problem solved

---
+++ very easy/00008powerCalculator.js
@@ -0,0 +1,22 @@
+/**Power Calculator
+Create a function that takes voltage and current and returns the calculated power.
+
+Examples
+circuitPower(230, 10) ➞ 2300
+
+circuitPower(110, 3) ➞ 330
+
+circuitPower(480, 20) ➞ 9600
+Notes
+Requires basic calculation of electrical circuits (see Resources for info).*/
+
+function circuitPower(force, velocity) {
+  let power = force * velocity;
+  return power;
+}
+
+console.log(circuitPower(230, 10));
+
+console.log(circuitPower(110, 3));
+
+console.log(circuitPower(480, 20));
