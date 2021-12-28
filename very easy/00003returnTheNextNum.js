e6aee0a2863d6382bf5fc460d8cdf9c38c99ec6d
Torjuman <torjumanrimon7@gmail.com>
2021-12-28 15:12:50 +0600

    Third problem solved

---
+++ very easy/00003returnTheNextNum.js
@@ -0,0 +1,8 @@
+function returnTheNextNum(numberPassed) {
+  let result = numberPassed + 1;
+  return result;
+}
+
+console.log(returnTheNextNum(0));
+console.log(returnTheNextNum(9));
+console.log(returnTheNextNum(-3));
