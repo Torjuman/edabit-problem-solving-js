a03cba4068cafb736207e193d72ac7fd5e7338f9
Torjuman <torjumanrimon7@gmail.com>
2021-12-28 15:34:40 +0600

    Third problem solved

--- very easy/00003returnTheNextNum.js
+++ very easy/00003returnTheNextNum.js
@@ -1,3 +1,17 @@
+/**Return the Next Number from the Integer Passed
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+Examples
+addition(0) ➞ 1
+
+addition(9) ➞ 10
+
+addition(-3) ➞ -2
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.*/
+
 function returnTheNextNum(numberPassed) {
   let result = numberPassed + 1;
   return result;
