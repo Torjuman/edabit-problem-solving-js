86a15f69e899716b55afa1acbcfa77dcfc48c17a
Torjuman <torjumanrimon7@gmail.com>
2021-12-29 12:14:48 +0600

    Twenty five number problem solved

---
+++ very easy/00025footballPoints.js
@@ -0,0 +1,27 @@
+/**Football Points
+Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far.
+
+wins get 3 points
+draws get 1 point
+losses get 0 points
+Examples
+footballPoints(3, 4, 2) ➞ 13
+
+footballPoints(5, 0, 2) ➞ 15
+
+footballPoints(0, 0, 1) ➞ 0
+Notes
+Inputs will be numbers greater than or equal to 0.*/
+
+function footballPoints(wins, draws, losses) {
+  let winningPoints = wins * 3,
+    drawPoints = draws * 1,
+    loosingPoints = losses * 0;
+  return winningPoints + drawPoints + loosingPoints;
+}
+
+console.log(footballPoints(3, 4, 2));
+
+console.log(footballPoints(5, 0, 2));
+
+console.log(footballPoints(0, 0, 1));
