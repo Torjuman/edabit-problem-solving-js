61c23d568055c70cfe33456f63765b3bf3cef3c8
Torjuman <torjumanrimon7@gmail.com>
2021-12-30 14:43:57 +0600

    Forty two number problem solved

---
+++ very easy/00042multipleOf100.js
@@ -0,0 +1,23 @@
+/*Multiple of 100
+Create a function that takes an integer and returns true if it's divisible by 100, otherwise return false.
+
+Examples
+divisible(1) ➞ false
+
+divisible(1000) ➞ true
+
+divisible(100) ➞ true
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.*/
+
+function divisible(num) {
+  return num % 100 === 0;
+}
+
+console.log(divisible(1));
+
+console.log(divisible(1000));
+
+console.log(divisible(100));
