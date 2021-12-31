7a550c60799c211628fbeb1635229b7744162910
Torjuman <torjumanrimon7@gmail.com>
2021-12-31 15:40:20 +0600

    Fifty four number problem solved

---
+++ very easy/00054upDownvotes.js
@@ -0,0 +1,21 @@
+/*Upvotes vs Downvotes
+Given an object containing counts of both upvotes and downvotes, return what vote count should be displayed. This is calculated by subtracting the number of downvotes from upvotes.
+
+Examples
+getVoteCount({ upvotes: 13, downvotes: 0 }) ➞ 13
+
+getVoteCount({ upvotes: 2, downvotes: 33 }) ➞ -31
+
+getVoteCount({ upvotes: 132, downvotes: 132 }) ➞ 0
+Notes
+You can expect only positive integers for vote counts.*/
+
+function getVoteCount(obj) {
+  return obj.upvotes - obj.downvotes;
+}
+
+console.log(getVoteCount({ upvotes: 13, downvotes: 0 }));
+
+console.log(getVoteCount({ upvotes: 2, downvotes: 33 }));
+
+console.log(getVoteCount({ upvotes: 132, downvotes: 132 }));
