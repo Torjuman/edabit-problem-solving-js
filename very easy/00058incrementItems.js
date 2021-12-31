4def9b587bc50d09c118c988fe2e61d7cd2c7466
Torjuman <torjumanrimon7@gmail.com>
2021-12-31 22:39:35 +0600

    Fifty eight number problem solved

---
+++ very easy/00058incrementItems.js
@@ -0,0 +1,28 @@
+/*Fix the Bug: Simple Array Manipulation
+Help fix all the bugs in the function incrementItems! It is intended to add 1 to every element in the array!
+
+Examples
+incrementItems([0, 1, 2, 3]) ➞ [1, 2, 3, 4]
+
+incrementItems([2, 4, 6, 8]) ➞ [3, 5, 7, 9]
+
+incrementItems([-1, -2, -3, -4]) ➞ [0, -1, -2, -3]
+Notes
+Make sure to read every line carefully.
+
+function incrementItems(arr) {
+	for (let i = 0; i < array.length; i++)
+		arr[i] === arr[i] + 1
+	return array
+}*/
+
+function incrementItems(arr) {
+  for (let i = 0; i < arr.length; i++) arr[i] = arr[i] + 1;
+  return arr;
+}
+
+console.log(incrementItems([0, 1, 2, 3]));
+
+console.log(incrementItems([2, 4, 6, 8]));
+
+console.log(incrementItems([-1, -2, -3, -4]));
