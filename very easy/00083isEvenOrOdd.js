a84cd81b6b3a36124a59a359d792816a1e0f03ee
Torjuman <torjumanrimon7@gmail.com>
2022-01-02 12:49:11 +0600

    Eighty three number problem solved

---
+++ very easy/00083isEvenOrOdd.js
@@ -0,0 +1,26 @@
+/*Is the Number Even or Odd?
+Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers.
+
+Examples
+isEvenOrOdd(3) ➞ "odd"
+
+isEvenOrOdd(146) ➞ "even"
+
+isEvenOrOdd(19) ➞ "odd"
+Notes
+Dont forget to return the result.
+Input will always be a valid integer.
+Expect negative integers (whole numbers).
+Tests are case sensitive (return "even" or "odd" in lowercase).*/
+
+function isEvenOrOdd(num) {
+  return num % 2 === 0 ? "even" : "odd";
+}
+
+console.log(isEvenOrOdd(3));
+
+console.log(isEvenOrOdd(146));
+
+console.log(isEvenOrOdd(19));
+
+console.log(isEvenOrOdd(-12));
